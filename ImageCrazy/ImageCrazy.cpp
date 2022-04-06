22c318218d59aef047f83d7fd77774c2107d513a
SCE_Maker <59202883+botcollig@users.noreply.github.com>
2022-04-06 19:39:10 +0800

    Add files via upload
    Many icons appear on the desktop, which is harmless to your computer!

---
+++ ImageCrazy/ImageCrazy.cpp
@@ -0,0 +1,31 @@
+﻿#include<tchar.h>
+#include<Windows.h>
+#include<ctime>
+using namespace std;
+int main()
+{
+
+	HWND hwnd = GetDesktopWindow();
+	HINSTANCE hShell32 = LoadLibrary(_T("SCEIMAGERESOU.dll"));//IMAGE.dll是我编译的，你也可以使用qingnse.dll
+	HDC hdc = GetWindowDC(hwnd);
+	srand(time(0));
+	Sleep(1000);
+	//POINT point;
+	while (1)
+	{
+		for (int x = 0; x <= 1920; x += 25)
+		{
+			for (int y = 0; y <= 1080; y += 25)
+			{
+				DrawIcon(hdc, x, y, LoadIcon(hShell32, MAKEINTRESOURCE(rand() % 256)));//hShell32.dll为默认
+
+			}
+
+		}
+		Sleep(1000);
+
+	}
+	while (1);
+	return 0;
+
+}
